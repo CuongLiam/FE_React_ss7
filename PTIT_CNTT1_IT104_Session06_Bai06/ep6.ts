dce0df74e56365e77250acc4b5dfa71b5ee195f6
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-08-26 22:25:04 +0700

    ep6

---
+++ PTIT_CNTT1_IT104_Session06_Bai06/ep6.ts
@@ -0,0 +1,79 @@
+abstract class Account{
+    id : string;
+    userName : string;
+    private password : string;  
+    isLogin : boolean;
+    role : string;
+
+    constructor(id : string, userName : string, password : string, isLogin : boolean, role : string){
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.isLogin = isLogin;
+        this.role = role;
+    }
+
+    abstract login() : void;
+
+    getPassword(){
+        return this.password;
+    }
+    logOut(){
+        if(this.isLogin == true){
+            this.isLogin = false;
+            console.log("Logout thanh cong");
+        }
+    }
+}
+
+class UserAcc extends Account{
+    status : string;
+
+    constructor(id : string, userName : string, password : string, isLogin : boolean, role : string, status : string){
+        super(id, userName, password, isLogin, role);
+        this.status = status;
+    }
+
+    //override 
+    login(): void{
+        if(this.status === "active"){
+            this.isLogin = true;
+            console.log("Login thanh cong");
+        } else{
+            console.log("Tai khoan bi khoa");
+        }
+    }
+}
+
+class Admin extends Account{
+    constructor(id : string, userName : string, password : string, isLogin : boolean, role : string){
+        super(id, userName, password, isLogin, role);
+    }
+
+    //override
+    login(): void{
+        this.isLogin = true;
+        console.log("Login thanh cong");
+    }
+
+    banUser(userAcc : UserAcc){
+        userAcc.status = "banned";
+        console.log(`tai khoan ${userAcc.userName} bi ban`);
+        
+    }
+}
+
+let userAcc1 = new UserAcc("123456789", "Nguyen Van A", "123456", false, "user", "active");
+// console.log(userAcc1.isLogin);
+// userAcc1.login();
+// console.log(userAcc1.isLogin);
+
+// userAcc1.logOut();
+// console.log(userAcc1.isLogin);
+
+console.log(userAcc1.status);
+
+let adminAcc1 = new Admin("123456789", "Admin", "123456", false, "admin");
+// adminAcc1.login();
+adminAcc1.banUser(userAcc1);
+console.log(userAcc1.status);
